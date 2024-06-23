5632732f01b0bb4fa33fb0f1f726a10a977b94cf
palak porwal <99019333+39palakved@users.noreply.github.com>
2024-06-23 10:53:56 +0530

    Time: 2 ms (25.25%), Space: 41.7 MB (10.61%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -1,9 +1,9 @@
 class Solution {
     public int minAddToMakeValid(String s) {
-        if (s.length()==0) {
+        if(s.length()<1){
             return 0;
         }
-        Stack <Character> st = new Stack <>();
+        Stack<Character> st = new Stack<>();
         for(int i=0; i<s.length();i++){
             if(s.charAt(i)=='('){
                 st.push(s.charAt(i));
@@ -15,11 +15,7 @@ class Solution {
                 st.push(s.charAt(i));
             }
         }
-        int cnt =0;
-        while(!st.isEmpty()){
-            cnt++;
-            st.pop();
-        }
-        return cnt;
+        return st.size();
+        
     }
 }
