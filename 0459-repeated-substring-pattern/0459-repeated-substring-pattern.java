36535c6c3cbcafaaca24c7c25fa7c102e5eb75ed
palak porwal <99019333+39palakved@users.noreply.github.com>
2024-06-27 12:59:16 +0530

    Time: 147 ms (15.09%), Space: 45.4 MB (15.61%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+  int n= s.length();
+        for(int i=1; i<=n/2;i++){
+           if(n%i==0){
+           if(s.replaceAll(s.substring(0,i),"").isEmpty()){
+            return true;
+        }
+           }
+          
+       }
+       return false; 
+    }
+}
