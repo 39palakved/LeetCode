39e9a34eeafe4f71b28d94b6056c91d15f67ac86
palak porwal <99019333+39palakved@users.noreply.github.com>
2024-11-06 15:06:27 +0530

    Time: 1 ms (99.46%), Space: 41.9 MB (64.90%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.java
+++ 3110-score-of-a-string/3110-score-of-a-string.java
@@ -3,7 +3,7 @@ class Solution {
         char ch[] = s.toCharArray();
         int sum =0;
         for(int i=1; i<ch.length;i++){
-            sum+=Math.abs((int)ch[i]-(int)ch[i-1]);
+            sum+=Math.abs(ch[i]-ch[i-1]);
         }
         return sum;
     }
