fc0afb569df84733b05aafe8facee19872b48d2a
palak porwal <99019333+39palakved@users.noreply.github.com>
2024-12-20 11:17:41 +0530

    Time: 1200 ms (5.01%), Space: 45.1 MB (14.92%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
@@ -1,31 +1,26 @@
 class Solution {
-    public int beautySum(String s) {
-        int sum =0;
-        int min = 10000; 
-        int max =0;
     
-        for(int i=0; i<s.length();i++){
-              HashMap<Character,Integer> map = new HashMap<>();
-            for(int j=i+1; j<=s.length();j++){
-                 max =0;
-                min=10000;
-              
-                map.put(s.charAt(j-1),map.getOrDefault(s.charAt(j-1),0)+1);
-                if(map.size()==1){
-                    continue;
-                }
-                
-                for(char key:map.keySet()){
-                    min= Math.min(min,map.get(key));
-                    max = Math.max(max,map.get(key));
-                }
-                    sum+=(max-min);
-                
-                
-              
-                
-            }
-        }
-      return sum;
+    public int beautySum(String s) {
+           int sum =0;
+           int min = Integer.MAX_VALUE;
+           int max = Integer.MIN_VALUE;
+       for (int i=0; i<s.length();i++){
+            HashMap<Character,Integer> map = new HashMap<>();
+          for(int j=i; j<s.length();j++){
+               min = Integer.MAX_VALUE;
+               max = Integer.MIN_VALUE;  
+              map.put(s.charAt(j),map.getOrDefault(s.charAt(j),0)+1);
+              if(map.size()==1){
+                  continue;
+              }
+              for(char k :map.keySet()){
+                  min = Math.min(min,map.get(k));
+                  max = Math.max(max,map.get(k));
+              }
+              sum+=max-min;
+           }
+       }
+        return sum ;
     }
+    
 }
